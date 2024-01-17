a7b0b493d7f5fd25b0e74604022699fbd4d68a98
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-01-17 13:02:28 -0800

    Create and style the ShoppingBagEmpty component

--- src/components/ShoppingBagEmpty/ShoppingBagEmpty.jsx
+++ src/components/ShoppingBagEmpty/ShoppingBagEmpty.jsx
@@ -1,3 +1,19 @@
-const ShoppingBagEmpty = () => <h1>Your bag</h1>;
+import { Link } from 'react-router-dom';
+import styles from './ShoppingBagEmpty.module.css';
+
+const ShoppingBagEmpty = () => (
+  <main className={styles.main}>
+    <h1>Your bag</h1>
+    <p>There are no items in your bag.</p>
+    <div className={styles.links}>
+      <Link className={styles.linkDark} to="/men">
+        SHOP MEN
+      </Link>
+      <Link className={styles.linkDark} to="/women">
+        SHOP WOMEN
+      </Link>
+    </div>
+  </main>
+);
 
 export default ShoppingBagEmpty;
