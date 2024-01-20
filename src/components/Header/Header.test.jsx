44a5ffbd85f7dc17c46e217b7f95c712dd34992b
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-01-20 10:37:39 -0800

    Add navigation tests for the Header and HomePage components

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,45 +1,70 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { renderWithRouter, setRoutes } from '../../tests/test-utils';
 import Header from './Header';
+import App from '../App/App';
+import MenPage from '../MenPage/MenPage';
+import WomenPage from '../WomenPage/WomenPage';
+import ShoppingBagEmpty from '../ShoppingBagEmpty/ShoppingBagEmpty';
 
-describe('Header component', () => {
+describe('rendered elements of the header', () => {
   it('renders the logo', () => {
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>
-    );
-    expect(
-      screen.getByRole('img', { name: 'Shopping bag icon' })
-    ).toBeInTheDocument();
+    renderWithRouter(<Header />);
+    expect(screen.getByRole('img', { name: /home page/i })).toBeInTheDocument();
   });
 
   it('renders the "Men" nav link', () => {
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>
-    );
+    renderWithRouter(<Header />);
     expect(screen.getByRole('link', { name: 'Men' })).toBeInTheDocument();
   });
 
   it('renders the "Women" nav link', () => {
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>
-    );
+    renderWithRouter(<Header />);
     expect(screen.getByRole('link', { name: 'Women' })).toBeInTheDocument();
   });
 
   it('renders the shopping bag icon', () => {
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>
-    );
+    renderWithRouter(<Header />);
     expect(
       screen.getByRole('img', { name: 'My shopping bag' })
     ).toBeInTheDocument();
+  });
+});
+
+describe('navigation of links to correct route', () => {
+  it('renders the home page when the logo is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/', <App />));
+    const link = screen.getByRole('link', { name: /home page/i });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      /home page/i
+    );
+  });
+
+  it('renders the men shopping page when the nav link is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/men', <MenPage />));
+    const link = screen.getByRole('link', { name: 'SHOP MEN' });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      'Men'
+    );
+  });
+
+  it('renders the women shopping page when the nav link is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/women', <WomenPage />));
+    const link = screen.getByRole('link', { name: 'SHOP WOMEN' });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      'Women'
+    );
+  });
+
+  it('renders the empty shopping bag page when the nav link is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/bag', <ShoppingBagEmpty />));
+    const link = screen.getByRole('link', { name: /bag/i });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      'Your bag'
+    );
   });
 });
--- src/components/HomePage/HomePage.test.jsx
+++ src/components/HomePage/HomePage.test.jsx
@@ -1,25 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { renderWithRouter, setRoutes } from '../../tests/test-utils';
 import HomePage from './HomePage';
+import MenPage from '../MenPage/MenPage';
+import WomenPage from '../WomenPage/WomenPage';
+import App from '../App/App';
 
-describe('HomePage component', () => {
+describe('rendered elements of the home page', () => {
   it('renders the "SHOP MEN" nav link', () => {
-    render(
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    );
+    renderWithRouter(<HomePage />);
     expect(screen.getByRole('link', { name: 'SHOP MEN' })).toBeInTheDocument();
   });
 
   it('renders the "SHOP WOMEN" nav link', () => {
-    render(
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    );
+    renderWithRouter(<HomePage />);
     expect(
       screen.getByRole('link', { name: 'SHOP WOMEN' })
     ).toBeInTheDocument();
+  });
+});
+
+describe('navigation of links to correct route', () => {
+  it('renders the men shopping page when the nav link is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/men', <MenPage />));
+    const link = screen.getByRole('link', { name: 'SHOP MEN' });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      'Men'
+    );
+  });
+
+  it('renders the women shopping page when the nav link is clicked', async () => {
+    renderWithRouter(setRoutes('/', <App />, '/women', <WomenPage />));
+    const link = screen.getByRole('link', { name: 'SHOP WOMEN' });
+    await userEvent.click(link);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      'Women'
+    );
   });
 });
